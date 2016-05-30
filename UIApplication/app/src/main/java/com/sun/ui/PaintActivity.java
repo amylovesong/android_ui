867bb7c3add635d3c39fac0926946b60a16d7ddf
sunxiaoling <amylovesong.sun@gmail.com>
2016-05-30 23:25:00 +0800

    Use Path class to draw sth

--- UIApplication/app/src/main/java/com/sun/ui/PaintActivity.java
+++ UIApplication/app/src/main/java/com/sun/ui/PaintActivity.java
@@ -2,12 +2,28 @@ package com.sun.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.ViewGroup;
+
+import com.sun.ui.paint.PathView;
 
 public class PaintActivity extends AppCompatActivity {
+    private ViewGroup mPathViewContainer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_paint);
+        mPathViewContainer = (ViewGroup) findViewById(R.id.activity_paint_path_view_container);
+
+        addPathView();
+    }
+
+    private void addPathView() {
+        PathView pathView = new PathView(getApplicationContext());
+        ViewGroup.LayoutParams lp = new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.MATCH_PARENT);
+        pathView.setLayoutParams(lp);
+
+        mPathViewContainer.addView(pathView);
     }
 }
---
+++ UIApplication/app/src/main/java/com/sun/ui/paint/PathView.java
@@ -0,0 +1,120 @@
+package com.sun.ui.paint;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.graphics.RectF;
+import android.util.AttributeSet;
+import android.widget.TextView;
+
+/**
+ * Custom view that use path to draw sth
+ */
+public class PathView extends TextView {
+
+    public PathView(Context context) {
+        super(context);
+    }
+
+    public PathView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public PathView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        final Paint paint = new Paint();
+        paint.setColor(Color.WHITE);
+        paint.setStyle(Paint.Style.STROKE);
+        paint.setStrokeWidth(5);
+
+        final Path path = new Path();
+
+        /** Line */
+//        useLineTo(path);
+//        useMoveTo(path);
+//        useClose(path);
+
+        /** Bezier curve */
+//        useQuadTo(path);
+//        useCubicTo(path);
+
+        /** Arc */
+//        useArcTo(path);
+
+        /** Relative method */
+//        useRLineTo(path);
+
+        /** Add method */
+//        useAddArc(path);
+        useAddOval(path);
+
+        canvas.drawPath(path, paint);
+
+        // Used for Path.Direction
+//        paint.setStyle(Paint.Style.FILL);
+        paint.setTextSize(100);
+        canvas.drawTextOnPath("123456789", path, 0, 0, paint);
+    }
+
+    private void useAddOval(Path path) {
+        path.moveTo(300, 300);
+        RectF oval = new RectF(300, 300, 800, 1000);
+//        path.addOval(oval, Path.Direction.CW);
+        path.addOval(oval, Path.Direction.CCW);
+    }
+
+    private void useAddArc(Path path) {
+        path.moveTo(300, 300);
+        path.lineTo(500, 500);
+        RectF oval = new RectF(300, 300, 800, 1000);
+        path.addArc(oval, 0, 90);
+    }
+
+    private void useRLineTo(Path path) {
+        path.moveTo(300, 300);
+        path.rLineTo(500, 500);// (500, 500) is relative to (300, 300) not (0, 0)
+    }
+
+    private void useArcTo(Path path) {
+        path.moveTo(300, 300);
+
+        RectF oval = new RectF(300, 300, 800, 800);
+//        path.arcTo(oval, 0, 90);
+        path.arcTo(oval, 0, 90, true);
+    }
+
+    private void useCubicTo(Path path) {
+        path.moveTo(300, 300);
+        path.cubicTo(500, 500, 800, 0, 1000, 300);
+    }
+
+    private void useQuadTo(Path path) {
+        path.moveTo(300, 300);
+        path.quadTo(500, 500, 800, 300);
+    }
+
+    private void useClose(Path path) {
+        useMoveTo(path);
+        path.close();
+    }
+
+    private void useLineTo(Path path) {
+        path.lineTo(300, 300);
+    }
+
+    private void useMoveTo(Path path) {
+        path.moveTo(300, 300);
+        path.lineTo(800, 300);
+        path.lineTo(1000, 600);
+        path.lineTo(500, 600);
+    }
+
+
+}
